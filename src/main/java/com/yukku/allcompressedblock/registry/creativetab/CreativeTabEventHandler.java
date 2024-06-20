91e518ee089957e322b9ecc47a2623bcfc34f015
yukkukomei <yukkukomei@gmail.com>
2024-06-21 01:05:18 +0900

    configでfalseにしたアイテムはクリエイティブタブに表示しないように変更

---
+++ src/main/java/com/yukku/allcompressedblock/registry/creativetab/CreativeTabEventHandler.java
@@ -0,0 +1,31 @@
+package com.yukku.allcompressedblock.registry.creativetab;
+
+import com.yukku.allcompressedblock.config.CompressedBlockConfig;
+import com.yukku.allcompressedblock.item.CompressedItem;
+import com.yukku.allcompressedblock.main.AllCompressedBlock;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod.EventBusSubscriber(modid = AllCompressedBlock.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CreativeTabEventHandler {
+    @SubscribeEvent
+    public static void onCreativeTabBuildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == CreativeTabs.COMPRESSED_BLOCK.get()) {
+            removeItems(event, CompressedBlockConfig.REGISTER_DIAMOND.get(), CompressedItem.COMPRESSED_DIAMOND_BLOCK_ITEMS);
+            removeItems(event, CompressedBlockConfig.REGISTER_IRON.get(), CompressedItem.COMPRESSED_IRON_BLOCK_ITEMS);
+        }
+    }
+
+    // アイテムをクリエイティブタブから削除
+    private static void removeItems(BuildCreativeModeTabContentsEvent event, boolean isDisplay, RegistryObject<Item>[] items) {
+        if (!isDisplay) {
+            for (RegistryObject<Item> item : items) {
+                event.getEntries().remove(new ItemStack(item.get()));
+            }
+        }
+    }
+}
